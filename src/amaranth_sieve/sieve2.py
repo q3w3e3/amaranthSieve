effcf64cd74240b3caea72f1a0bdb570baf349e6
q3w3e3 <charlotte.woodrow@q3w3e3.dev>
2024-06-18 20:09:01 -0700

    begin writing Memory?

--- src/amaranth_sieve/sieve2.py
+++ src/amaranth_sieve/sieve2.py
@@ -23,48 +23,83 @@ class TopLevel(Elaboratable):
 
 
 class Sieve(Component):
-    isPrimeArray: Out(maxN, init=~0)
+    isPrimeArray: Out(maxN, init=~0) ## LETS STOP DOING THIS 
     done: Out(1)
     reset: In(1)
 
+    def __init__(self):
+        super().__init__()
+        self.pos = Signal(bits_to_store)
+        self.prime = Signal.like(self.pos)
+
     def elaborate(self, platform):
         m = Module()
-        pos = Signal(bits_to_store)
-        prime = Signal.like(pos)
+
+        m.submodules.memory = self.memory
 
         with m.FSM():
             with m.State("INIT"):
                 m.d.sync += self.isPrimeArray.eq(~1)
                 m.d.comb += self.done.eq(0)
-                m.d.sync += [pos.eq(0), prime.eq(0)]
+                m.d.sync += [self.pos.eq(0), self.prime.eq(0)]
                 with m.If(~self.reset):
                     m.next = "STEPPING"
             with m.State("STEPPING"):
-                m.d.sync += pos.eq(pos + 1)
-                with m.If(pos <= len(self.isPrimeArray)):
+                m.d.sync += self.pos.eq(self.pos + 1)
+                with m.If(self.pos <= len(self.isPrimeArray)):
                     m.next = "CHECKING"
                 with m.Else():
                     m.next = "DONE"
             with m.State("CHECKING"):
-                with m.If(self.isPrimeArray.bit_select(abs(pos - 1), 1)):
-                    m.d.sync += prime.eq(pos)
+                with m.If(self.isPrimeArray.bit_select(abs(self.pos - 1), 1)):
+                    m.d.sync += self.prime.eq(self.pos)
                     m.next = "STEPBYP"
                 with m.Else():
                     m.next = "STEPPING"
             with m.State("STEPBYP"):
-                with m.If(prime > int(len(self.isPrimeArray) / 2)):
+                with m.If(self.prime > int(len(self.isPrimeArray) / 2)):
                     m.next = "DONE"
-                with m.Elif((pos + prime) <= len(self.isPrimeArray)):
-                    m.d.sync += pos.eq(pos + prime)
+                with m.Elif((self.pos + self.prime) <= len(self.isPrimeArray)):
+                    m.d.sync += self.pos.eq(self.pos + self.prime)
                     m.next = "FLIPPING"
                 with m.Else():
-                    m.d.sync += pos.eq(prime)
+                    m.d.sync += self.pos.eq(self.prime)
                     m.next = "STEPPING"
             with m.State("FLIPPING"):
-                m.d.sync += self.isPrimeArray.bit_select(abs(pos - 1), 1).eq(0)
+                m.d.sync += self.isPrimeArray.bit_select(abs(self.pos - 1), 1).eq(0)
                 m.next = "STEPBYP"
             with m.State("DONE"):
                 m.d.comb += self.done.eq(1)
                 with m.If(self.reset):
                     m.next = "INIT"
         return m
+
+
+class PrimeMemory(Component):
+    n: In(1)
+    wr_enable: In(1)
+    read: Out(1)
+    write: In(1)
+
+    def __init__(self, bits):
+        super().__init__()
+        self.memory = Memory(width=128,depth=-(-bits//128))
+        self.wr_port = self.memory.write_port(granularity=1) # is this worth doing? 
+        self.r_port = self.memory.read_port(transparent_for=(self.wr_port,)) #does the read need to be transparent for the writes? does this matter? maybe comb domain?
+
+    def elaborate(self, platform):
+        """
+            each clock cycle
+            if wr_enable: 
+                comb: self.wr_port.en.eq( 1<< n%width ) #off by 1? # am i understanding the use of the Wider Enable correctly?
+                sync:
+                      self.wr_port.addr.eq( (ceiling n/width) ) #off by 1?
+                      self.wr_port.data.eq(self.write)
+            else:
+                comb:
+                      self.read.eq( self.r_port.addr.eq( ceiling n/width )[ 1<< n%width ] )
+
+            this feels inefficient because if im reading/writing successive bits im waiting for the entire row each time? does this matter?
+        """
+
+
