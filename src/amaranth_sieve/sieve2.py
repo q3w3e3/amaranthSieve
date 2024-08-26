e1dbdbd0e990d9dbdeb7c2e92550d1058a265ef5
q3w3e3 <charlotte.woodrow@q3w3e3.dev>
2024-08-26 21:27:04 +0100

    closer i think lol

--- src/amaranth_sieve/sieve2.py
+++ src/amaranth_sieve/sieve2.py
@@ -2,9 +2,8 @@ from amaranth import *
 from amaranth.lib.wiring import Component, In, Out
 from amaranth.lib.memory import Memory
 
-bits_to_store = 12
+bits_to_store = 8
 maxN = min(1000000, (2**bits_to_store) - 1)
-
 
 class TopLevel(Elaboratable):
     def elaborate(self, platform):
@@ -24,7 +23,7 @@ class TopLevel(Elaboratable):
 
 
 class Sieve(Component):
-    isPrimeArray: Out(maxN, init=~0) ## LETS STOP DOING THIS 
+    #PrimeArray: Out(maxN, init=~0) ## LETS STOP DOING THIS 
     done: Out(1)
     reset: In(1)
 
@@ -33,40 +32,64 @@ class Sieve(Component):
         self.pos = Signal(bits_to_store)
         self.prime = Signal.like(self.pos)
 
+        self.PrimeArray = PrimeMemory(8,bits_to_store) # Mess with width for Optimizaiton.
+
     def elaborate(self, platform):
         m = Module()
 
+        m.submodules += self.PrimeArray
+
+        m.d.comb += [ 
+                    self.PrimeArray.write.eq(0),
+                    self.PrimeArray.wr_enable.eq(0)
+                    ]
+        
+        m.d.comb += self.PrimeArray.n.eq(self.pos)
+
+
+
         with m.FSM():
             with m.State("INIT"):
-                m.d.sync += self.isPrimeArray.eq(~1)
+                ## TODO add way to init the memory, presently it is set to all 1 by the init above
+
                 m.d.comb += self.done.eq(0)
                 m.d.sync += [self.pos.eq(0), self.prime.eq(0)]
                 with m.If(~self.reset):
                     m.next = "STEPPING"
+
             with m.State("STEPPING"):
                 m.d.sync += self.pos.eq(self.pos + 1)
-                with m.If(self.pos <= len(self.isPrimeArray)):
+                with m.If(self.pos <= maxN):
                     m.next = "CHECKING"
                 with m.Else():
                     m.next = "DONE"
+
             with m.State("CHECKING"):
-                with m.If(self.isPrimeArray.bit_select(abs(self.pos - 1), 1)):
+                #with m.If(self.PrimeArray.bit_select(abs(self.pos - 1), 1)): # cant do this anymore 
+                with m.If(self.PrimeArray.read):
                     m.d.sync += self.prime.eq(self.pos)
                     m.next = "STEPBYP"
                 with m.Else():
                     m.next = "STEPPING"
+
             with m.State("STEPBYP"):
-                with m.If(self.prime > int(len(self.isPrimeArray) / 2)):
+                with m.If(self.prime > int(len(self.PrimeArray) / 2)):
                     m.next = "DONE"
-                with m.Elif((self.pos + self.prime) <= len(self.isPrimeArray)):
+                with m.Elif((self.pos + self.prime) <= len(self.PrimeArray)):
                     m.d.sync += self.pos.eq(self.pos + self.prime)
                     m.next = "FLIPPING"
                 with m.Else():
                     m.d.sync += self.pos.eq(self.prime)
                     m.next = "STEPPING"
+
             with m.State("FLIPPING"):
-                m.d.sync += self.isPrimeArray.bit_select(abs(self.pos - 1), 1).eq(0)
+                ## this is the fun bit 
+                m.d.comb += [ 
+                    self.PrimeArray.write.eq(0),
+                    self.PrimeArray.wr_enable.eq(1)
+                ]
                 m.next = "STEPBYP"
+
             with m.State("DONE"):
                 m.d.comb += self.done.eq(1)
                 with m.If(self.reset):
@@ -74,37 +97,68 @@ class Sieve(Component):
         return m
 
 
-class PrimeMemory(Component): ## THIS DOESNT QUITE WORK AS IS, READS LAG BEHIND ADDR BY ONE CYCLE 
+class PrimeMemory(Component): ## TODO: add a way to re-init this ?
     n: In(bits_to_store)
     wr_enable: In(1)
     write: In(1)
     read: Out(1)
+    ready: Out(1)
 
-    def __init__(self, bits):
+    def __init__(self, width, bits):
         super().__init__()
-        self.memory = Memory(shape=8,depth=3,init=b"\xff\x00\x77")
-        self.wr_port = self.memory.write_port()
-        self.r_port = self.memory.read_port(domain="comb") #does the read need to be transparent for the writes? does this matter? maybe comb domain?
+
+        depth = -(-bits//width)
+
+        self.memory = Memory(shape=width,depth=depth,init=[~1]+([~0]*(depth-1)))
+        self.wr_port = self.memory.write_port(granularity=1) # I get choice of either 1 granularity here, or transparency below
+        self.rd_port = self.memory.read_port() #does the read need to be transparent for the writes? does this matter?
+
+        self.position = Signal.like(self.n)
+        self.row = Signal(self.memory.depth)
+        self.offset = Signal(self.memory.shape)
+
+    def __len__(self):
+        return self.memory.depth * self.memory.shape
 
     def elaborate(self, platform): 
         m = Module()
 
         m.submodules += self.memory
 
-        row = self.n//self.memory.shape
-        offset = self.n % self.memory.shape
+        prev_row = Signal.like(self.row)
 
-        m.d.comb += self.r_port.addr.eq(row)
+        m.d.sync += prev_row.eq(self.row)
 
-        m.d.comb += self.wr_port.en.eq(self.wr_enable)
-        m.d.comb += self.read.eq(self.r_port.data.bit_select(offset,1))
+        m.d.comb += self.position.eq(self.n-1)
 
+        m.d.comb += self.row.eq(self.position // self.memory.shape)
+        m.d.comb += self.offset.eq(self.position % self.memory.shape)
+        m.d.comb += self.rd_port.addr.eq(self.row)
 
+        m.d.comb += self.wr_port.en.eq(self.wr_enable << self.offset) # if use transparancy, remove this shift (maybe)
+        
         m.d.comb += [
-            self.wr_port.addr.eq( row ),
-            self.wr_port.data.eq(self.r_port.data | (1 << offset))
+            self.wr_port.addr.eq(self.row),
+            #self.wr_port.data.eq( ( self.rd_port.data & ~( 1 << self.offset )) | ( self.write << self.offset))
+            self.wr_port.data.eq(0)
         ]
 
+
+        ## the purpose of the below was to handle state during the 1 cycle delay on reading a new row
+        with m.FSM():
+            with m.State("BECOMING_UNREADY"):
+
+                m.d.comb += self.ready.eq(1)
+
+                m.d.comb += self.read.eq(self.rd_port.data.bit_select(self.offset,1))
+
+                with m.If(self.row != prev_row):
+                    m.d.comb += self.ready.eq(0)
+                    m.next = "BECOMING_READY"
+                    
+            with m.State("BECOMING_READY"):
+                m.d.comb += self.ready.eq(1) ##  im just... like... waiting a cycle for the state change and then going back ready.... that feels Bad?
+                m.next = "BECOMING_UNREADY"
         
         return m 
 
--- tests/test_sieve.py
+++ tests/test_sieve.py
@@ -3,49 +3,58 @@ from amaranth.sim import Simulator
 from amaranth_sieve.sieve2 import Sieve as Sieve2
 from amaranth_sieve.sieve2 import PrimeMemory
 
-# def test_sieve2():
-#     sieve2 = Sieve2()
-
-#     async def testbench(ctx):
-#         while ctx.get(~sieve2.done):
-#             await ctx.tick()
-
-
-#     sim2 = Simulator(sieve2)
-#     sim2.add_clock(period=1.667e-7)
-#     sim2.add_testbench(testbench)
-#     with sim2.write_vcd("sieve2.vcd"):
-#         sim2.run()
-
-def test_memory():
-    memory = PrimeMemory(256)
+def test_sieve2():
+    sieve2 = Sieve2()
 
     async def testbench(ctx):
-        ctx.set(memory.write,1)
-        for n in range(24):
-            ctx.set(memory.n,n)
+        while ctx.get(~sieve2.done):
             await ctx.tick()
-        await ctx.tick().repeat(10)
-        for n in range(24):
-            ctx.set(memory.n,n)
-            #await ctx.tick() ## if i dont wait here then my Write will use the last row read, which isnt fine when changing rows
-            ctx.set(memory.wr_enable,1)
-            ctx.set(memory.write,1)
-            await ctx.tick()
-        ctx.set(memory.wr_enable,0)
-        await ctx.tick().repeat(10)
-        for n in range(24):
-            ctx.set(memory.n,n)
-            await ctx.tick()
-        await ctx.tick().repeat(10)
-        # for n in range(24):
-        #     ctx.set(memory.n,n)
-        #     ctx.set(memory.wr_enable,1)
-        #     ctx.set(memory.write,0)
-        #     await ctx.tick().repeat(1)
 
-    sim = Simulator(memory)
-    sim.add_clock(period=8.33e-8)
-    sim.add_testbench(testbench)
-    with sim.write_vcd("memory.vcd"):
-        sim.run()
+
+    sim2 = Simulator(sieve2)
+    sim2.add_clock(period=1.667e-7)
+    sim2.add_testbench(testbench)
+    with sim2.write_vcd("sieve2.vcd"):
+        sim2.run_until(1.667e-7 * 200)
+
+# def test_memory():
+#     memory = PrimeMemory(8,24)
+
+#     async def testbench(ctx):
+#         for n in range(24):
+#             while ctx.get(~memory.ready):
+#                 await ctx.tick()
+#             ctx.set(memory.n,n)
+#         await ctx.tick().repeat(10)
+
+#         ctx.set(memory.write,0)
+#         ctx.set(memory.wr_enable,1)
+#         for n in range(24):
+#             while ctx.get(~memory.ready):
+#                 await ctx.tick()
+#             await ctx.tick()
+#             ctx.set(memory.n,n)
+#         await ctx.tick().repeat(10)
+
+#         ctx.set(memory.wr_enable,1)
+#         for n in range(24):
+#             ctx.set(memory.write,n%2)
+#             while ctx.get(~memory.ready):
+#                 await ctx.tick()
+#             await ctx.tick()
+#             ctx.set(memory.n,n)
+#         ctx.set(memory.wr_enable,0)
+#         await ctx.tick().repeat(10)
+
+#         for n in range(24):
+#             while ctx.get(~memory.ready):
+#                 await ctx.tick()
+#             ctx.set(memory.n,n)
+#             await ctx.tick()
+#         await ctx.tick().repeat(10)
+
+#     sim = Simulator(memory)
+#     sim.add_clock(period=8.33e-8)
+#     sim.add_testbench(testbench)
+#     with sim.write_vcd("memory.vcd"):
+#         sim.run()
